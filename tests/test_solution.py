d4a397100725543fdb80a030df2303861e38476d
G M Jubayer Zaman <gmjz.back@gmail.com>
2025-01-12 00:46:57 +0600

    Update test_solution.py

--- tests/test_solution.py
+++ tests/test_solution.py
@@ -1,1 +1,10 @@
+from solutions import user_solution
+
+def test_add():
+    assert user_solution.add(2, 3) == 5
+    assert user_solution.add(10, 5) == 15
+
+def test_subtract():
+    assert user_solution.subtract(10, 5) == 5
+    assert user_solution.subtract(20, 4) == 16
 
